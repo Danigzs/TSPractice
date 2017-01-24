582d734d56ab4385f3b2ceda537322f0868e1bdb
Danigzs <dangzs94@gmail.com>
2017-01-24 13:03:15 -0600

    mi primer commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {NavbarComponent} from './navbar/navbar.component';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  
+    <h1>Hello Angular 2 Routing!</h1>
+    <h2>Testing angular</h2>
+    
+      `,
+})
+export class AppComponent  {}
