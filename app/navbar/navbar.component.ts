70824b3477d9ccf953d2f3e97927ed180bd4ed70
Danigzs <dangzs94@gmail.com>
2017-01-25 11:36:00 -0600

    mi primer commit

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -14,17 +14,26 @@ import { Component } from '@angular/core';
       </button>
       <a class="navbar-brand" href="#">WebSiteName</a>
     </div>
+
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-folder-close"> Cotizaciones </span><span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Page 1-1</a></li>
+            <li><a href="http://www.w3schools.com/css/css_navbar.asp">Page 1-1</a></li>
             <li><a href="#">Page 1-2</a></li>
             <li><a href="#">Page 1-3</a></li>
           </ul>
         </li>
+
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-folder-open"> Inventario </span><span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="http://www.w3schools.com/css/css_navbar.asp">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>      
+        </li>
+
         <li><a href="#">Page 2</a></li>
         <li><a href="#">Page 3</a></li>
       </ul>     
