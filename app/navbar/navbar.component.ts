288de02b721f2b429a7e86437e3270f482b02e48
Danigzs <dangzs94@gmail.com>
2017-01-26 12:27:18 -0600

    mi primer commit

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -76,8 +76,8 @@ import { Component } from '@angular/core';
 
                 	<h1>Datos del articulo</h1>
         		<form action="demo_form.asp">
- 				 	First name: <input type="text" name="fname"><br>
-  				 	Last name: <input type="text" name="lname"><br>
+ 				 First name: <input type="text" name="fname"><br>
+  				 Last name: <input type="text" name="lname"><br>
   					<input type="submit" value="Submit">
 				</form>
 
