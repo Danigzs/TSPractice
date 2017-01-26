40b90c625e53d7cd3cde8738491d3f7f8ed49830
Danigzs <dangzs94@gmail.com>
2017-01-26 13:10:53 -0600

    mi primer commit

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -19,7 +19,7 @@ import { Component } from '@angular/core';
       <ul class="nav navbar-nav">
         <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-folder-close"> Cotizaciones </span><span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="http://www.w3schools.com/js/js_window_screen.asp" target="_blank">link text</a></li>
+            <li><a href="#" data-toggle="modal" data-target="#modal2">Nuevo</a></li>
             <li role="presentation" class="divider"></li>
             <li><a href="#">Page 1-2</a></li>
             <li role="presentation" class="divider"></li>
@@ -104,6 +104,43 @@ import { Component } from '@angular/core';
     </div>
 </div>
   
+<div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="modal2" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content"> 
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Basic Modal</h4>
+
+                	<h1>Datos del articulo</h1>
+        		<form action="demo_form.asp">
+ 				 First name: <input type="text" name="fname"><br>
+  				 Last name: <input type="text" name="lname"><br>
+  					<input type="submit" value="Submit">
+				</form>
+
+					<li>
+                        <input type="file" id="myfile" name="myfile" class="rm-input" onchange="selectedFile();"/></li>
+    				<li>
+						<div id="fileSize"></div></li>
+    				<li>
+						<div id="fileType"></div></li>
+    				<li>
+                        <input type="button" value="Subir Archivo" onClick="uploadFile()" class="rm-button" /></li>
+
+            </div>
+            <div class="modal-body">
+                <h3>Modal Body</h3>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">
+                    Save changes
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
   `,
   
 })
