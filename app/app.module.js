b6f82bc92fc94afe5254965f84ba8c9b394191bf
juanrdzreyes <juan_rdz_157@hotmail.com>
2017-03-15 22:44:24 -0600

    random changes

--- app/app.module.js
+++ app/app.module.js
@@ -5,19 +5,22 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var core_1 = require("@angular/core");
-var platform_browser_1 = require("@angular/platform-browser");
-var forms_1 = require("@angular/forms");
-var router_1 = require("@angular/router");
-var app_component_1 = require("./app.component");
-var navbar_component_1 = require("./navbar/navbar.component");
-var form_component_1 = require("./form.component");
-var clientes_details_1 = require("./clientes.details");
-var cotizador_component_1 = require("./cotizador.component");
-var inventario_component_1 = require("./inventario.component");
-var dialog_producto_component_1 = require("./modals/dialog.producto.component");
-var dialog_inventario_component_1 = require("./modals/dialog.inventario.component");
-var material_1 = require("@angular/material");
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var platform_browser_1 = require('@angular/platform-browser');
+var forms_1 = require('@angular/forms');
+var router_1 = require('@angular/router');
+var app_component_1 = require('./app.component');
+var navbar_component_1 = require('./navbar/navbar.component');
+var form_component_1 = require('./form.component');
+var clientes_details_1 = require('./clientes.details');
+var cotizador_component_1 = require('./cotizador.component');
+var inventario_component_1 = require('./inventario.component');
+var dialog_producto_component_1 = require('./modals/dialog.producto.component');
+var dialog_inventario_component_1 = require('./modals/dialog.inventario.component');
+var material_1 = require('@angular/material');
 var appRoutes = [
     { path: 'form-directive', component: form_component_1.FormComponent },
     { path: 'my-app', component: app_component_1.AppComponent },
@@ -30,30 +33,31 @@ var appRoutes = [
 var AppModule = (function () {
     function AppModule() {
     }
+    AppModule = __decorate([
+        core_1.NgModule({
+            imports: [platform_browser_1.BrowserModule,
+                router_1.RouterModule.forRoot(appRoutes), forms_1.FormsModule,
+                material_1.MaterialModule.forRoot()
+            ],
+            declarations: [
+                navbar_component_1.NavbarComponent,
+                app_component_1.AppComponent,
+                form_component_1.FormComponent,
+                clientes_details_1.ClientesComponent,
+                cotizador_component_1.CotizadorComponent,
+                inventario_component_1.InventarioComponent,
+                dialog_producto_component_1.DialogProductoComponent,
+                dialog_inventario_component_1.DialogInventarioComponent
+            ],
+            entryComponents: [
+                dialog_producto_component_1.DialogProductoComponent,
+                dialog_inventario_component_1.DialogInventarioComponent,
+            ],
+            bootstrap: [navbar_component_1.NavbarComponent,]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
     return AppModule;
 }());
-AppModule = __decorate([
-    core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule,
-            router_1.RouterModule.forRoot(appRoutes), forms_1.FormsModule,
-            material_1.MaterialModule.forRoot()
-        ],
-        declarations: [
-            navbar_component_1.NavbarComponent,
-            app_component_1.AppComponent,
-            form_component_1.FormComponent,
-            clientes_details_1.ClientesComponent,
-            cotizador_component_1.CotizadorComponent,
-            inventario_component_1.InventarioComponent,
-            dialog_producto_component_1.DialogProductoComponent,
-            dialog_inventario_component_1.DialogInventarioComponent
-        ],
-        entryComponents: [
-            dialog_producto_component_1.DialogProductoComponent,
-            dialog_inventario_component_1.DialogInventarioComponent,
-        ],
-        bootstrap: [navbar_component_1.NavbarComponent,]
-    })
-], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
--- app/modals/dialog.producto.component.ts
+++ app/modals/dialog.producto.component.ts
@@ -3,7 +3,8 @@ import {MdDialog, MdDialogConfig, MdDialogRef} from '@angular/material';
 
 @Component({
   selector: 'modal-producto-selector',
-  templateUrl: "./app/modals/dialog_producto.html"
+  templateUrl: "./app/modals/dialog_producto.html",
+  styleUrls: ["./app/modals/dialog_producto.css"],
 })
 export class DialogProductoComponent {
     constructor(public dialogRef: MdDialogRef<DialogProductoComponent>) { }
