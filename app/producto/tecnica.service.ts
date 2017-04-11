26a532f596514573574bbfc4748ee9898eb7e4e3
Danigzs <dangzs94@gmail.com>
2017-04-11 09:47:44 -0500

    fix it

--- app/producto/tecnica.service.ts
+++ app/producto/tecnica.service.ts
@@ -7,15 +7,13 @@ export class TecnicaService {
     constructor(){
         var tecnica1= new Tecnica();
         var tecnica2= new Tecnica();
-        var tecnica3= new Tecnica();
-        var tecnica4= new Tecnica();
+      
 
         tecnica1.setTecnica("Bordado",150);
         tecnica2.setTecnica("Serigrafia",50);
-        tecnica3.setTecnica("Tampografia",60)
-        tecnica4.setTecnica("Vinil",80)
 
-        this.tecnicas = [tecnica1,tecnica2,tecnica3,tecnica4];
+
+        this.tecnicas = [tecnica1,tecnica2];
     }
     getTecnicas() {
     return this.tecnicas;
